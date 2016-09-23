a66845d706c8c5ddeddb96f227b9c478b5a7c8a9
Glebov Boris <bglebov@ussc.ru>
2016-09-23 21:16:55 +0500

    added bulma framework and gulp tasks

--- gui/gulpfile.js
+++ gui/gulpfile.js
@@ -5,6 +5,8 @@
 var gulp = require('gulp');
 
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
 
 gulp.task('sass', function () {
     return gulp.src('scss/*.scss')
@@ -12,4 +14,21 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('bulma', function () {
+   return gulp.src('node_modules/bulma/bulma.sass')
+       .pipe(sass())
+       .pipe(gulp.dest('css'))
+});
+
+gulp.task('concat-css', function () {
+   return gulp.src("css/*.css")
+       .pipe(concat("styles.css"))
+       .pipe(gulp.dest("css"))
+});
+
+gulp.task('clean', function () {
+    return gulp.src("css", {read: false})
+        .pipe(clean());
+});
+
+gulp.task('default', ['clean', 'sass', 'concat-css', 'bulma']);
