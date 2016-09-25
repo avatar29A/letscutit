2b05e3808530912c5f3caabb3ddf90c6e13afa25
Glebov Boris <avatar29A@gmail.com>
2016-09-25 20:01:21 +0500

    - Recreate layout.

--- gui/app/dropbox/dropbox.component.ts
+++ gui/app/dropbox/dropbox.component.ts
@@ -3,37 +3,55 @@
  */
 
 import {Component, OnInit,
-        trigger, style, state, transition, animate} from "@angular/core";
+    trigger, style, state, transition, animate} from "@angular/core";
 import {Router} from "@angular/router";
-
-enum DropboxState {
-    Prepare = 1,
-    Dragover,
-}
 
 @Component(
     {
         selector: 'dropbox',
         templateUrl: 'app/dropbox/dropbox.template.html',
-        styleUrls: ['css/dropbox.css']
+        styleUrls: ['css/dropbox.css'],
+        animations: [
+            trigger('dropboxState', [
+                state('idle', style({
+                    border: '0px solid white'
+                })),
+
+                state('dragover', style({
+                    border: '6px solid #66A366'
+                })),
+                transition('idle => dragover', animate('100ms ease-in')),
+                transition('dragover => idle', animate('100ms ease-out'))
+            ])
+        ]
     }
 )
 export class DropboxComponent {
-    state :DropboxState;
+    state:string;
 
     constructor(private router:Router) {
-        this.state = DropboxState.Prepare;
+        this.state = 'idle';
     }
 
-    fileDragHover(e: DragEvent): void {
+    fileDragHover(e:DragEvent):void {
         e.stopPropagation();
         e.preventDefault();
 
-        this.state = (e.type == "dragover" ? DropboxState.Dragover : DropboxState.Prepare);
-        console.log("State: " + this.state);
+        this.state = (e.type == "dragover" ? "dragover" : "idle");
     }
 
-    fileSelectHandler(e: DragEvent): void {
-        console.log(e);
+    fileSelectHandler(e:DragEvent):void {
+        // cancel event and hover styling
+        this.fileDragHover(e);
+
+        // fetch FileList object
+        var files = e.dataTransfer.files;
+        for (var file of files) {
+            this.processMp3File(file);
+        }
+    }
+
+    processMp3File(file:File):void {
+        console.log("Get: " + file.name);
     }
 }
---
+++ gui/app/dropbox/dropbox.exceptions.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by Warlock on 25.09.2016.
+ */
+
+declare class ErrorClass implements Error {
+    public name: string;
+    public message: string;
+    constructor(message?: string);
+}
+
+class UnsupportedAudioFormatError extends ErrorClass {
+    public name = "UnsupportedAudioFormatError";
+    constructor (public message?: string) {
+        if (message) {
+            super(message);
+        }else {
+            super("Unsupported format. Expected mp3 file.");
+        }
+    }
+}
