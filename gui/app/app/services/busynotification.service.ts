051634f522645dffe5f394076bf04ab416aae9a2
Glebov Boris <bglebov@ussc.ru>
2016-10-04 19:53:19 +0500

    - Maked progress animation from 0 to 100%

--- gui/app/app/services/busynotification.service.ts
+++ gui/app/app/services/busynotification.service.ts
@@ -2,10 +2,10 @@
  * Created by Warlock on 03.10.2016.
  */
 
-import { Injectable } from '@angular/core';
-import { Subject }    from 'rxjs/Subject';
+import {Injectable} from '@angular/core';
+import {Subject}    from 'rxjs/Subject';
 
-class BusyNotifyConfig {
+export class BusyNotifyConfig {
     constructor(public state:boolean, public isShowSpinner, public progress:number = 0) {
     }
 }
@@ -16,8 +16,16 @@ export class BusyNotificationService {
 
     applicationBusyTurnOnOff$ = this.applicationBusyTurnOnOffSource.asObservable();
 
-    busyTurnOn(isShowSpinner) {
-        this.applicationBusyTurnOnOffSource.next(new BusyNotifyConfig(true, isShowSpinner));
+    busyTurnOn(isShowSpinner:boolean = false) {
+        this.applicationBusyTurnOnOffSource.next(new BusyNotifyConfig(true, isShowSpinner, 25));
+    }
+
+    progressUpTo(progress:number, isShowSpinner:boolean = false):void {
+        this.applicationBusyTurnOnOffSource.next(new BusyNotifyConfig(true, isShowSpinner, progress))
+    }
+
+    progressUp10(isShowSpinner:boolean = false) {
+        this.progressUpTo(10, isShowSpinner);
     }
 
     busyTurnOff() {
--- gui/app/trends/trends.component.ts
+++ gui/app/trends/trends.component.ts
@@ -3,11 +3,35 @@
  */
 
 import {Component} from "@angular/core";
+import {BusyNotificationService} from "../app/services/busynotification.service";
 
 @Component({
     selector: 'about',
-    template: "<h2>Trends</h2>"
+    template: "<button class='button is-prime' (click)='startAwait()'>Start Busy</button>" +
+    "<button class='button is-prime' (click)='stopAwait()'>Stop Busy</button>" +
+    "<button class='button is-prime' (click)='set100()'>Set 100</button>"
 })
 export class TrendsComponent {
+    progress:number = 0;
 
+    constructor(private busyNotificator:BusyNotificationService) {
+    }
+
+    startAwait():void {
+        this.progress += 10;
+        if (this.progress > 100) {
+            this.progress = 0;
+        }
+
+        this.busyNotificator.progressUpTo(this.progress);
+    }
+
+    stopAwait():void {
+        this.progress = 0;
+        this.busyNotificator.busyTurnOff();
+    }
+
+    set100():void {
+        this.busyNotificator.progressUpTo(100);
+    }
 }
